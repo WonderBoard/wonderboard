d2bb76c1472771bcee9de33411d0f06a161800cf
Peter Wright <pwright93@gmail.com>
2020-10-13 13:16:03 -0700

    for now build file will be in root

---
+++ build/precache-manifest.bdc456c75a79f0a5e91de6ac8e420a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caeb123cc885c0f9eabc7bcc67623ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf7feb57227ca8c438f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e3fb8a164505189f83d",
+    "url": "/static/js/2.433cc17d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.433cc17d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf7feb57227ca8c438f",
+    "url": "/static/js/main.4d2d7ea5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
